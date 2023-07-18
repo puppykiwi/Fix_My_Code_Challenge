352decb384b6259f2a2bd44bcb2cc904e36c51bb
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-18 12:03:50 +0300

    bump

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class Square:
+class square:
     
     width = 0
     height = 0
@@ -21,7 +21,7 @@ class Square:
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
